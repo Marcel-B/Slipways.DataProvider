f384000b39f808b736a484e51a4636febf011f9b
Marcel Benders <marcel.benders@icloud.com>
2019-12-20 19:21:40 +0100

    (dev) Add Services

---
+++ Slipways.DataProvider/Infrastructure/SecretProvider.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using Microsoft.Extensions.FileProviders;
+
+namespace Slipways.DataProvider.Infrastructure
+{
+    public interface ISecretProvider
+    {
+        string GetSecret(string key);
+    }
+    public class SecretProvider : ISecretProvider
+    {
+
+        public string GetSecret(
+              string key)
+        {
+            const string DOCKER_SECRET_PATH = "/run/secrets/";
+            if (Directory.Exists(DOCKER_SECRET_PATH))
+            {
+                using var provider = new PhysicalFileProvider(DOCKER_SECRET_PATH);
+                var fileInfo = provider.GetFileInfo(key);
+                if (fileInfo.Exists)
+                {
+                    using var stream = fileInfo.CreateReadStream();
+                    using var streamReader = new StreamReader(stream);
+                    return streamReader.ReadToEnd();
+                }
+            }
+            return string.Empty;
+        }
+    }
+}
---
+++ Slipways.DataProvider/Services/CacheLoader.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Slipways.DataProvider.Services
+{
+    public class CacheLoader : IHostedService
+    {
+        private readonly IServiceProvider _services;
+        private readonly ILogger<CacheLoader> _logger;
+        private Timer _timer;
+
+        public CacheLoader(
+            IServiceProvider services,
+            ILogger<CacheLoader> logger)
+        {
+            _services = services;
+            _logger = logger;
+        }
+
+        public Task StartAsync(
+            CancellationToken stoppingToken)
+        {
+            _logger.LogInformation("CacheLoader service running");
+
+            _timer = new Timer(DoWork, null, TimeSpan.Zero,
+                TimeSpan.FromMinutes(1));
+            return Task.CompletedTask;
+        }
+
+        private async void DoWork(
+            object state)
+        {
+            //try
+            //{
+            //    using var scope = _services.CreateScope();
+            //    _logger.LogInformation("Reload cache");
+            //    var cache = scope.ServiceProvider.GetRequiredService<IMemoryCache>();
+            //    var rep = scope.ServiceProvider.GetRequiredService<IRepositoryWrapper>();
+
+            //    cache.Remove(Cache.Waters);
+            //    _ = await rep.Water.SelectAllAsync();
+
+            //    cache.Remove(Cache.Manufacturer);
+            //    _ = await rep.Manufacturer.SelectAllAsync();
+
+            //    cache.Remove(Cache.Stations);
+            //    _ = await rep.Station.SelectAllAsync();
+
+            //    cache.Remove(Cache.Extras);
+            //    _ = await rep.Extra.SelectAllAsync();
+            //}
+            //catch (Exception e)
+            //{
+            //    _logger.LogError(6666, $"Error while updating cache", e);
+            //}
+        }
+
+        public Task StopAsync(
+            CancellationToken stoppingToken)
+        {
+            _logger.LogInformation("CacheLoader Service is stopping.");
+            _timer?.Change(Timeout.Infinite, 0);
+            return Task.CompletedTask;
+        }
+
+        public void Dispose()
+        {
+            _timer?.Dispose();
+        }
+    }
+}
