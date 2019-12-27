7fb8aa6f22bee40ca94a313a8da9fe9f116298d3
Marcel Benders <marcel.benders@icloud.com>
2019-12-27 18:00:12 +0100

    (dev) Update Caching

--- Slipways.DataProvider/Contracts/IInitializer.cs
+++ Slipways.DataProvider/Contracts/IInitializer.cs
@@ -5,7 +5,7 @@ namespace com.b_velop.Slipways.DataProvider.Contracts
 {
     public interface IInitializer
     {
-        Task Init<T>(string path, string name) where T : class, IEntity;
+        Task InitDatabase<T>(string path, string name) where T : class, IEntity;
         Task InitCache<T>(string name) where T : class, IEntity;
     }
 }
--- Slipways.DataProvider/Services/CacheLoader.cs
+++ Slipways.DataProvider/Services/CacheLoader.cs
@@ -1,10 +1,9 @@
 ﻿using System;
 using System.Threading;
 using System.Threading.Tasks;
-using com.b_velop.Slipways.Data.Contracts;
-using com.b_velop.Slipways.Data.Extensions;
 using com.b_velop.Slipways.Data.Helper;
-using Microsoft.Extensions.Caching.Distributed;
+using com.b_velop.Slipways.Data.Models;
+using com.b_velop.Slipways.DataProvider.Contracts;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -25,15 +24,28 @@ namespace com.b_velop.Slipways.DataProvider.Services
             _logger = logger;
         }
 
-        public Task StartAsync(
+        public async Task StartAsync(
             CancellationToken stoppingToken)
         {
             _logger.LogInformation("CacheLoader service running");
+            await InitDatabaseAsync();
+            _timer = new Timer(DoWork, null, TimeSpan.FromSeconds(15), TimeSpan.FromMinutes(1));
+        }
 
-            _timer = new Timer(DoWork, null, TimeSpan.FromSeconds(3),
-                TimeSpan.FromMinutes(1));
-
-            return Task.CompletedTask;
+        private async Task InitDatabaseAsync()
+        {
+            _logger.LogInformation("Add Values to Database");
+            using var scope = _services.CreateScope();
+            var initializer = scope.ServiceProvider.GetRequiredService<IInitializer>();
+            await initializer.InitDatabase<Water>("./initWaters.json", Cache.Waters);
+            await initializer.InitDatabase<Extra>("./initExtras.json", Cache.Extras);
+            await initializer.InitDatabase<Manufacturer>("./initManufacturers.json", Cache.Manufacturers);
+            await initializer.InitDatabase<Slipway>("./initSlipways.json", Cache.Slipways);
+            await initializer.InitDatabase<Service>("./initServices.json", Cache.Services);
+            await initializer.InitDatabase<SlipwayExtra>("./initSlipwayExtras.json", Cache.SlipwayExtras);
+            await initializer.InitDatabase<Station>("./initStations.json", Cache.Stations);
+            await initializer.InitDatabase<ManufacturerService>("./initManufacturerServices.json", Cache.ManufacturerServices);
+            _logger.LogInformation("Add Values to Database done");
         }
 
         private async void DoWork(
@@ -43,13 +55,18 @@ namespace com.b_velop.Slipways.DataProvider.Services
             {
                 using var scope = _services.CreateScope();
                 _logger.LogInformation("Reload cache");
-                var cache = scope.ServiceProvider.GetRequiredService<IDistributedCache>();
-                var rep = scope.ServiceProvider.GetRequiredService<IRepositoryWrapper>();
+                var initializer = scope.ServiceProvider.GetRequiredService<IInitializer>();
 
-                var waters = await rep.Water.SelectAllAsync();
-                var o = waters.ToByteArray();
-                await cache.RemoveAsync(Cache.Waters);
-                await cache.SetAsync(Cache.Waters, o);
+                await initializer.InitCache<Water>(Cache.Waters);
+                await initializer.InitCache<SlipwayExtra>(Cache.SlipwayExtras);
+                await initializer.InitCache<Slipway>(Cache.Slipways);
+                await initializer.InitCache<Extra>(Cache.Extras);
+                await initializer.InitCache<Station>(Cache.Stations);
+                await initializer.InitCache<Manufacturer>(Cache.Manufacturers);
+                await initializer.InitCache<ManufacturerService>(Cache.ManufacturerServices);
+                await initializer.InitCache<Service>(Cache.Services);
+
+                _logger.LogInformation("Reload cache done");
             }
             catch (Exception e)
             {
