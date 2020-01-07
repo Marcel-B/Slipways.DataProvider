9be9ec734bc92f1a03ba8bf8d57897ca77ca267b
Marcel Benders <marcel.benders@icloud.com>
2020-01-07 08:25:42 +0100

    (dev) Bugfix

--- Slipways.DataProvider/Migrations/20200106143803_AddPorts.cs
+++ Slipways.DataProvider/Migrations/20200106143803_AddPorts.cs
@@ -10,7 +10,7 @@ namespace com.b_velop.Slipways.DataProvider.Migrations
             migrationBuilder.AddColumn<Guid>(
                 name: "PortFk",
                 table: "Slipways",
-                nullable: false,
+                nullable: true,
                 defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
 
             migrationBuilder.CreateIndex(
